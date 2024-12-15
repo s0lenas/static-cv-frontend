c566591fa599254c94d9a4b179d06cea2da3856f
s0lenas <herkuskrisciunas@gmail.com>
2024-12-15 23:07:00 +0200

    Updated About page

--- cvsite/src/components/About.js
+++ cvsite/src/components/About.js
@@ -8,10 +8,16 @@ export const About = () => {
                 I am fulfilled by continuous learning, discovery, and improvement, fueled by an eagerness to express my creativity through various avenues. My extensive studies abroad in countries like the United Kingdom, United States, Belgium, and France have taught me how to adapt and communicate in different environments and cultures.
             </p>
             <p>
-                I find joy and motivation in fast-paced and collaborative settings and embrace difficult challenges with an open mind. My time at university has been a great tool for discovering my passions and deepening my knowledge in Computer Science, gaining knowledge of the IT field and proficiency in languages such as Python, SQL, Java, and C++.
+                I have always been motivated by the wonder of discovery and continuous learning. My time at university reflects this motivation - I took a wide range of modules ranging from networking and cybersecurity to software development and artificial intelligence, gaining a clear understanding of the IT landscape and providing myself with a solid foundation upon which to build my career. Having understood many different areas of IT, I most fell in love with cloud and security, as I am fascinated by figuring out how things work and how the IT infrastructure is built to function safely, effectively and efficiently.
             </p>
             <p>
-                As a cybersecurity and IT professional I have gained valuable knowledge of SIEM tools, Linux, common cybersecurity risks, along with strategies to mitigate them. I am continuing to build my skillset by studying the vast array of resources available for aspiring IT professionals, and I am excited to discover what awaits me.
+                As part of my time at university and my self-taught curriculum, I have extensively utilized tools such as Git, Microsoft AD, AWS cloud services, Splunk, C#, Python, Java and many more; I have used these tools to administer Windows servers, manage user accounts, investigate security alerts, automate service deployment tasks, setting up CI/CD, and more. Having gained an understanding of a wide range of tools used by IT and security professionals, I am capable of adapting, learning, and keeping up with the fast-moving pace of IT.
+            </p>
+            <p>
+                Although I decided to focus on cybersecurity after graduating, I have rediscovered my passion for cloud engineering, operations, and development. I am actively studying based on the syllabus for AWS Certified Developer - Associate certification using platforms such as KodeKloud and Coursera. I am also currently working on a personal project which involves creating a static website, deploying it using AWS serverless cloud infrastructure, and implementing CI/CD and IaC using GitHub Actions and Terraform respectively.
+            </p>
+            <p>
+                I strongly believe my experience in security is valuable to the field of cloud and devops. Because of my time at university and experience as a security professional, I have gained a strong technical foundation and an aptitude in analyzing and solving complex problems. I am passionate about discovery, which motivates me to be a continuous and attentive learner.
             </p>
         </section>
     );
